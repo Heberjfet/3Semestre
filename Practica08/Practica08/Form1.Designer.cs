9da67ef0c359eb11497a8593515660beaf4e2ad1
Heberjfet <heber.alvaro49@unach.mx>
2023-10-25 23:41:37 -0600

    practica sensor

---
+++ Practica08/Practica08/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Practica08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbConnection = new System.Windows.Forms.Label();
+            this.Conectar = new System.Windows.Forms.Button();
+            this.Desconectar = new System.Windows.Forms.Button();
+            this.tbLimit = new System.Windows.Forms.TextBox();
+            this.lbTemp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LIMITE DE TEMPERATURA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(418, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TEMPERATURA";
+            // 
+            // lbConnection
+            // 
+            this.lbConnection.AutoSize = true;
+            this.lbConnection.Location = new System.Drawing.Point(84, 233);
+            this.lbConnection.Name = "lbConnection";
+            this.lbConnection.Size = new System.Drawing.Size(0, 13);
+            this.lbConnection.TabIndex = 2;
+            // 
+            // Conectar
+            // 
+            this.Conectar.Location = new System.Drawing.Point(58, 300);
+            this.Conectar.Name = "Conectar";
+            this.Conectar.Size = new System.Drawing.Size(140, 58);
+            this.Conectar.TabIndex = 3;
+            this.Conectar.Text = "CONECTAR";
+            this.Conectar.UseVisualStyleBackColor = true;
+            // 
+            // Desconectar
+            // 
+            this.Desconectar.Location = new System.Drawing.Point(407, 268);
+            this.Desconectar.Name = "Desconectar";
+            this.Desconectar.Size = new System.Drawing.Size(135, 63);
+            this.Desconectar.TabIndex = 4;
+            this.Desconectar.Text = "DESCONECTAR";
+            this.Desconectar.UseVisualStyleBackColor = true;
+            // 
+            // tbLimit
+            // 
+            this.tbLimit.Location = new System.Drawing.Point(69, 136);
+            this.tbLimit.Name = "tbLimit";
+            this.tbLimit.Size = new System.Drawing.Size(118, 20);
+            this.tbLimit.TabIndex = 5;
+            // 
+            // lbTemp
+            // 
+            this.lbTemp.AutoSize = true;
+            this.lbTemp.Location = new System.Drawing.Point(452, 175);
+            this.lbTemp.Name = "lbTemp";
+            this.lbTemp.Size = new System.Drawing.Size(35, 13);
+            this.lbTemp.TabIndex = 6;
+            this.lbTemp.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 412);
+            this.Controls.Add(this.lbTemp);
+            this.Controls.Add(this.tbLimit);
+            this.Controls.Add(this.Desconectar);
+            this.Controls.Add(this.Conectar);
+            this.Controls.Add(this.lbConnection);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Temperatura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbConnection;
+        private System.Windows.Forms.Button Conectar;
+        private System.Windows.Forms.Button Desconectar;
+        private System.Windows.Forms.TextBox tbLimit;
+        private System.Windows.Forms.Label lbTemp;
+    }
+}
+
---
+++ Practica08/Practica08/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Practica08
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextDelegate(string value);
+
+        public SerialPort ArduinoPort { 
+            get;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM5"; // Check on your machine
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            ArduinoPort.Open(); // Uncomment this if you want to open the port
+
+            // Bind events
+            this.Conectar.Click += btnConectar_Click;
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = ArduinoPort.ReadLine();
+            WriteToTxt(data);
+        }
+
+        private void WriteToTxt(string data)
+        {
+            if (InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new SetTextDelegate(WriteToTxt), new object[] { data });
+                }
+                catch { }
+            }
+            else
+            {
+                lbTemp.Text = data;
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            Desconectar.Enabled = true;
+            //Conectar.Enabled = false;
+
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                {
+                    ArduinoPort.Open();
+                }
+
+                if (int.TryParse(tbLimit.Text, out int temperatureLimit)) // Assuming tbLimit is the TextBox for the temperature limit
+                {
+                    // Convert the value to a string and then to a byte array
+                    string limitString = temperatureLimit.ToString();
+                    ArduinoPort.Write(limitString);
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid numeric value in the TextBox for the temperature limit.");
+                }
+
+                lbConnection.Text = "Connection OK";
+                lbConnection.ForeColor = System.Drawing.Color.Lime;
+            }
+            catch
+            {
+                MessageBox.Show("Please check your communication port or disconnect.");
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            Conectar.Enabled = true;
+            Desconectar.Enabled = false;
+
+            if (ArduinoPort.IsOpen)
+                ArduinoPort.Close();
+
+            lbConnection.Text = "Disconnected";
+            lbConnection.ForeColor = System.Drawing.Color.Red;
+            lbTemp.Text = "00.0";
+        }
+    }
+}
