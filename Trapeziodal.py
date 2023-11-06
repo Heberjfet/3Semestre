d10f06e54764e066085327a40dd7e810ba9180d5
Heberjfet <114103375+Heberjfet@users.noreply.github.com>
2023-11-06 06:49:23 -0600

    vercion mejorada de tra

--- Trapeziodal.py
+++ Trapeziodal.py
@@ -2,7 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f(x):
+<<<<<<< HEAD
+    return x**3 + y
+
+# Definimos los límites de integración
+=======
     return x**3 
+>>>>>>> c52db537f4f1118eb1f39b27b9c800058bc747e5
 a = 0
 b = 1
 
