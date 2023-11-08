11cb9d172aebf89b445724b7ab11ba86410aa0e6
Heberjfet <heber.alvaro49@unach.mx>
2023-11-07 23:24:29 -0600

    servos

--- servos.py
+++ servos.py
@@ -1,30 +1,20 @@
 import serial
 import tkinter as tk
-from tkinter import Scale
 
-# Configura la conexión serial con el Arduino (ajusta el puerto según sea necesario)
-ser = serial.Serial('COM8', 9600)  # Cambia 'COM3' al puerto correcto
+# Configura el puerto serie
+puerto = serial.Serial('COM7', 9600)
 
-# Función para enviar el valor del slider al Arduino
-def enviar_valor(valor):
-    valor = int(valor)
-    ser.write(f'{valor}\n'.encode())
+def enviar_angulo(angulo):
+    # Envia el ángulo al Arduino
+    puerto.write('<{}>'.format(angulo).encode())
 
-# Crea una ventana
-ventana = tk.Tk()
-ventana.title("Control de Servomotor")
+# Crea la ventana de la aplicación
+root = tk.Tk()
+root.title("Control del Servomotor")
 
-# Crea un slider para controlar la posición del servo
-slider = Scale(ventana, from_=0, to=180, orient="horizontal", label="Ángulo del Servo")
-slider.pack()
+# Crea el control deslizante
+scale = tk.Scale(root, from_=0, to=180, orient='horizontal', command=enviar_angulo)
+scale.pack(fill='x')
 
-# Configura una función para enviar el valor cuando el slider se mueve
-slider.bind("<Motion>", lambda event: enviar_valor(slider.get()))
-
-# Cierra la conexión serial al cerrar la ventana
-def cerrar_ventana():
-    ser.close()
-    ventana.destroy()
-
-ventana.protocol("WM_DELETE_WINDOW", cerrar_ventana)
-ventana.mainloop()
+# Inicia la aplicación
+root.mainloop()
