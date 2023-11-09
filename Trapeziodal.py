63b3e25f75030f2f3b0d013ed72ec4cf0e81aa11
Heberjfet <heber.alvaro49@unach.mx>
2023-11-09 01:07:22 -0600

    codigo terminado

--- Trapeziodal.py
+++ Trapeziodal.py
@@ -1,22 +1,34 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-def f(x):
-    return x**3 + y
+def funcion(x):
+    return x**3 + 2
 
+def metodo_trapecio(a, b):
+    resultado = (b - a) * (funcion(a) + funcion(b)) / 2
+    return resultado
+
+# Intervalo de 0 a 1
 a = 0
 b = 1
 
-n = 10
+# Puntos para graficar la función
+x_vals = np.linspace(a, b, 100)
+y_vals = funcion(x_vals)
 
-x = np.linspace(a, b, n+1)
-y = f(x)
+# Calcular el área
+area = metodo_trapecio(a, b)
+print('Area = {:.2f}'.format(area))
 
-plt.plot(x, y, 'b')
-plt.fill_between(x, y, 0, alpha=0.2)
+# Graficar la función
+plt.plot(x_vals, y_vals, label='f(x) = x^3 + 2')
 
-area = np.trapz(y, x)
+# Imprimir el resultado en la gráfica
+plt.text(0.1, 1.5, 'Area = {:.2f}'.format(area), verticalalignment='bottom', horizontalalignment='right', color='black', fontsize=15)
 
-print("El área bajo la curva es:", area)
-
-input("Pulse enter para cerrar la ventana: ")
+# Trapecios utilizados en la aproximación
+plt.title('método del trapecio')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.legend()
+plt.show()
