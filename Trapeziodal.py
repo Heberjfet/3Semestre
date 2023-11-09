0ed525d53e397c8af4c8a639683648e39a622866
Heberjfet <heber.alvaro49@unach.mx>
2023-11-09 01:10:41 -0600

    mejora

--- Trapeziodal.py
+++ Trapeziodal.py
@@ -8,25 +8,19 @@ def metodo_trapecio(a, b):
     resultado = (b - a) * (funcion(a) + funcion(b)) / 2
     return resultado
 
-# Intervalo de 0 a 1
 a = 0
 b = 1
 
-# Puntos para graficar la función
 x_vals = np.linspace(a, b, 100)
 y_vals = funcion(x_vals)
 
-# Calcular el área
 area = metodo_trapecio(a, b)
 print('Area = {:.2f}'.format(area))
 
-# Graficar la función
 plt.plot(x_vals, y_vals, label='f(x) = x^3 + 2')
 
-# Imprimir el resultado en la gráfica
-plt.text(0.1, 1.5, 'Area = {:.2f}'.format(area), verticalalignment='bottom', horizontalalignment='right', color='black', fontsize=15)
+plt.text(0.1, 1.5, 'Area = {:.2f}'.format(area), verticalalignment='center', horizontalalignment='center', color='black', fontsize=15)
 
-# Trapecios utilizados en la aproximación
 plt.title('método del trapecio')
 plt.xlabel('x')
 plt.ylabel('f(x)')
